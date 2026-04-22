03687f40820765a514c35b39e78038abc831c766
Alex Palchyk <palchyk.alex99@gmail.com>
2026-04-22 12:17:42 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # mini-http-server
+
+A simple HTTP server written in C that serves a basic "Hello, World!" page.
+
+## Prerequisites
+
+- GCC compiler
+- Make (optional, for using Makefile)
+
+On Ubuntu/Debian, install with:
+```bash
+sudo apt update
+sudo apt install build-essential
+```
+
+## Building
+
+To build the server, run:
+
+```bash
+make
+```
+
+Or directly with gcc:
+```bash
+gcc -Wall -Wextra -std=c99 -o mini_http_server main.c
+```
+
+This will create an executable named `mini_http_server`.
+
+## Running
+
+To start the server, run:
+
+```bash
+./mini_http_server
+```
+
+The server will listen on port 8080. You can access it by opening a web browser and navigating to `http://localhost:8080`.
---
+++ main.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    char buffer[BUFFER_SIZE] = {0};
+    char *response = "HTTP/1.1 200 OK\r\n"
+                     "Content-Type: text/html\r\n"
+                     "Content-Length: 48\r\n"
+                     "\r\n"
+                     "<html><body><h1>Hello, World!</h1></body></html>";
+
+    // Create socket
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Bind to port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen
+    if (listen(server_fd, 3) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        // Accept connection
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Read request (though we don't use it for simplicity)
+        read(new_socket, buffer, BUFFER_SIZE);
+
+        // Send response
+        send(new_socket, response, strlen(response), 0);
+
+        // Close connection
+        close(new_socket);
+    }
+
+    return 0;
+}
